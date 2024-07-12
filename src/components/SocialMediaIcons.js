639387a345fa173653ea7c5e9a625cf18bfead54
Sahil Atul Darandale <sdarandale0699@gmail.com>
2024-07-12 17:11:01 -0400

    Update: Changed calendar icon and added social media links

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -6,13 +6,13 @@ import linkedinLogo from "../assets/images/linkedin_logo.png";
 export default function SocialMediaIcons() {
     return (
         <div className="flex space-x-4">
-            <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/SDev6699" target="_blank" rel="noopener noreferrer">
                 <img className="h-8 w-8" src={githubLogo} alt="Github Logo"/>
             </a>
-            <a href="https://twitter.com/your-username" target="_blank" rel="noopener noreferrer">
+            <a href="https://x.com/darandale72397" target="_blank" rel="noopener noreferrer">
                 <img className="h-8 w-8" src={twitterLogo} alt="Twitter Logo"/>
             </a>
-            <a href="https://linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/sahil-atul-darandale-16b0a31a0/" target="_blank" rel="noopener noreferrer">
                 <img className="h-8 w-8" src={linkedinLogo} alt="Linkedin Logo"/>
             </a>
         </div>
