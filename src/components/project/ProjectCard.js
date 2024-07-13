3c763844946bdb855c5e10bcd647b5a0e2e990e0
Sahil Atul Darandale <sdarandale0699@gmail.com>
2024-07-12 20:10:51 -0400

    Update Projects and ProjectCard components to center project cards and ensure 3 cards in a row; Update index.html to include new favicon

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -2,31 +2,36 @@ import React from 'react';
 import linkIcon from "../../assets/images/link.png";
 import githubLogo from "../../assets/images/github_logo.png";
 
-export default function ProjectCard({ imageSrc, title, description, techStack, livePreviewLink, viewCodeLink }) {
+export default function ProjectCard({ imageSrc, imageWidth, imageHeight, title, description, techStack, livePreviewLink, viewCodeLink }) {
     return (
-        <div className="rounded-lg shadow-lg overflow-hidden w-full max-w-[375px] h-auto mx-auto group">
-            <img
-                src={imageSrc}
-                alt="Project"
-                className="w-full h-48 object-cover"
-            />
-            <div className="p-4">
-                <h3 className="font-poppins text-xl md:text-2xl font-medium text-heading-text dark:text-[#CCCCCC] leading-26">{title}</h3>
-                <p className="text-text-gray dark:text-custom-gray mt-2 text-sm md:text-base">
-                    {description}
-                </p>
-                <p className="text-text-gray dark:text-custom-gray mt-4 text-sm md:text-base">
-                    <span className="font-semibold">Tech stack: </span>{techStack.join(', ')}
-                </p>
-                <div className="flex justify-between items-center mt-4">
-                    <a href={livePreviewLink} className="flex items-center opacity-0 transform translate-x-[-20px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 text-text-gray dark:text-custom-gray dark:hover:text-[#D9D9D9] hover:text-heading-text text-sm md:text-base">
-                        <img className="h-4 w-4 md:h-6 md:w-6 mr-1" src={linkIcon} alt="Link Logo" />
-                        Live Preview
-                    </a>
-                    <a href={viewCodeLink} className="flex items-center opacity-0 transform translate-x-[20px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 text-text-gray dark:text-custom-gray dark:hover:text-[#D9D9D9] hover:text-heading-text text-sm md:text-base">
-                        <img className="h-4 w-4 md:h-6 md:w-6 mr-1" src={githubLogo} alt="Github Logo" />
-                        View Code
-                    </a>
+        <div className="relative p-1 bg-gradient-to-r from-[#13B0F5] to-[#E70FAA] rounded-lg overflow-hidden w-full max-w-[375px] h-auto mx-auto group">
+            <div className="relative rounded-lg shadow-lg overflow-hidden bg-white dark:bg-gray-800 w-full h-full flex flex-col">
+                <div className="flex justify-center items-center" style={{ width: '100%', height: imageHeight }}>
+                    <img
+                        src={imageSrc}
+                        alt="Project"
+                        className="object-cover"
+                        style={{ maxWidth: '100%', maxHeight: '100%' }}
+                    />
+                </div>
+                <div className="p-4 flex flex-col flex-grow">
+                    <h3 className="font-poppins text-xl md:text-2xl font-medium text-heading-text dark:text-[#CCCCCC] leading-26">{title}</h3>
+                    <p className="text-text-gray dark:text-custom-gray mt-2 text-sm md:text-base flex-grow">
+                        {description}
+                    </p>
+                    <p className="text-text-gray dark:text-custom-gray mt-4 text-sm md:text-base">
+                        <span className="font-semibold">Tech stack: </span>{techStack.join(', ')}
+                    </p>
+                    <div className="flex justify-between items-center mt-4">
+                        <a href={livePreviewLink} className="flex items-center opacity-0 transform translate-x-[-20px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 text-text-gray dark:text-custom-gray dark:hover:text-[#D9D9D9] hover:text-heading-text text-sm md:text-base" target="_blank" rel="noopener noreferrer">
+                            <img className="h-4 w-4 md:h-6 md:w-6 mr-1" src={linkIcon} alt="Link Logo" />
+                            Live Preview
+                        </a>
+                        <a href={viewCodeLink} className="flex items-center opacity-0 transform translate-x-[20px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 text-text-gray dark:text-custom-gray dark:hover:text-[#D9D9D9] hover:text-heading-text text-sm md:text-base" target="_blank" rel="noopener noreferrer">
+                            <img className="h-4 w-4 md:h-6 md:w-6 mr-1" src={githubLogo} alt="Github Logo" />
+                            View Code
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ProjectCard from "./ProjectCard";
+import BiteStatsImage from '../../assets/images/BiteStats.png'; // Ensure this path is correct
 
 export default function Projects() {
     return (
         <div id="projects" className="flex flex-col items-center py-8 mx-auto min-h-screen w-screen max-w-6.8xl">
             <h1 className="font-poppins text-tech-title font-bold text-heading-text dark:text-[#CCCCCC] leading-26 text-center">Projects</h1>
             <h2 className="font-poppins text-2xl font-regular text-text-gray dark:text-custom-gray leading-26 mt-4 text-center">Things I've built so far</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 w-full px-4 md:px-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 w-full px-4 md:px-8 justify-center">
+                <ProjectCard
+                    imageSrc={BiteStatsImage}
+                    imageWidth="375px"
+                    imageHeight="200px"
+                    title="BiteStats"
+                    description="BiteStats is a Chrome extension designed to help users track and analyze their food delivery orders from Grubhub and DoorDash. It provides detailed insights and visualizations using charts and statistics."
+                    techStack={['React', 'Chart.js', 'Material-UI', 'Day.js']}
+                    livePreviewLink="https://chromewebstore.google.com/detail/ilmpifekkgnmlhkipkfckaockaobdfel/preview?hl=en-GB&authuser=0" // Add the actual link
+                    viewCodeLink="https://github.com/SDev6699/bitestats" // Add the actual link
+                />
                 <ProjectCard
                     imageSrc="https://via.placeholder.com/375x200" // Replace with your actual image source
+                    imageWidth="375px"
+                    imageHeight="200px"
                     title="Project Title goes here"
                     description="This is sample project description. Random things are here in description. This is a sample project lorem ipsum generator for dummy content."
                     techStack={['HTML', 'JavaScript', 'SASS', 'React']}
@@ -17,6 +30,18 @@ export default function Projects() {
                 />
                 <ProjectCard
                     imageSrc="https://via.placeholder.com/375x200" // Replace with your actual image source
+                    imageWidth="375px"
+                    imageHeight="200px"
+                    title="Project Title goes here"
+                    description="This is sample project description. Random things are here in description. This is a sample project lorem ipsum generator for dummy content."
+                    techStack={['HTML', 'JavaScript', 'SASS', 'React']}
+                    livePreviewLink="#"
+                    viewCodeLink="#"
+                />
+                {/* <ProjectCard
+                    imageSrc="https://via.placeholder.com/375x200" // Replace with your actual image source
+                    imageWidth="375px"
+                    imageHeight="200px"
                     title="Project Title goes here"
                     description="This is sample project description. Random things are here in description. This is a sample project lorem ipsum generator for dummy content."
                     techStack={['HTML', 'JavaScript', 'SASS', 'React']}
@@ -25,12 +50,24 @@ export default function Projects() {
                 />
                 <ProjectCard
                     imageSrc="https://via.placeholder.com/375x200" // Replace with your actual image source
+                    imageWidth="375px"
+                    imageHeight="200px"
                     title="Project Title goes here"
                     description="This is sample project description. Random things are here in description. This is a sample project lorem ipsum generator for dummy content."
                     techStack={['HTML', 'JavaScript', 'SASS', 'React']}
                     livePreviewLink="#"
                     viewCodeLink="#"
                 />
+                <ProjectCard
+                    imageSrc="https://via.placeholder.com/375x200" // Replace with your actual image source
+                    imageWidth="375px"
+                    imageHeight="200px"
+                    title="Project Title goes here"
+                    description="This is sample project description. Random things are here in description. This is a sample project lorem ipsum generator for dummy content."
+                    techStack={['HTML', 'JavaScript', 'SASS', 'React']}
+                    livePreviewLink="#"
+                    viewCodeLink="#"
+                /> */}
             </div>
         </div>
     );
